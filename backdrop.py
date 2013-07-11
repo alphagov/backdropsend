b81c51ad272a73bb7387d954715ac19922900cd1
Max Fliri <mfliri@thoughtworks.com>
2013-07-11 12:53:33 +0100

    Parse arguments with the desired CLI interface
    The application now accepts url and token as command line options, and
    reads data either from an argument file or from stdin
    
    @gtrogers, @maxfliri

--- backdrop.py
+++ backdrop.py
@@ -1,8 +1,32 @@
+import os
+from pprint import pprint
 import requests
+import argparse
+import sys
 
 
-def send(args, input):
-    requests.post(url=args[0], data=input.read(), headers={
-        "Authorization": "Bearer " + args[1],
+def parse_args(args, input):
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--url', help="URL of the target bucket",
+                        required=True)
+    parser.add_argument('--token', help="Bearer token for the target bucket",
+                        required=True)
+    parser.add_argument('file', help="File containing JSON to send", nargs='?',
+                        type=argparse.FileType('r'),
+                        default=input)
+    return parser.parse_args(args)
+
+
+def send(args, input=None):
+    arguments = parse_args(args, input)
+
+    data = arguments.file.read()
+
+    requests.post(url=arguments.url, data=data, headers={
+        "Authorization": "Bearer " + arguments.token,
         "Content-type": "application/json"
     })
+
+
+if __name__ == "__main__":
+    send(sys.argv[1:], input=sys.stdin)
--- tests/functional/test_backdrop_send.py
+++ tests/functional/test_backdrop_send.py
@@ -1,4 +1,6 @@
 from io import StringIO
+import os
+import tempfile
 import unittest
 import mock
 import backdrop
@@ -8,9 +10,28 @@ class TestBackdropSend(unittest.TestCase):
 
     @mock.patch('requests.post')
     def test_it_posts_data_to_bucket_url_with_auth_token(self, post):
-        input = StringIO(u'{"key": "value"}')
-        backdrop.send(["http://backdrop-bucket-url", "bucket-auth-token"], input)
 
+        f = tempfile.NamedTemporaryFile(suffix=".json")
+        f.write('{"key": "value"}')
+        f.flush()
+
+        backdrop.send(["--url", "http://backdrop-bucket-url",
+                       "--token", "bucket-auth-token",
+                       f.name])
+
+        self.assert_data_got_sent(post)
+
+    @mock.patch('requests.post')
+    def test_it_reads_data_from_stdin_to_post_to_backdrop(self, post):
+
+        data_to_send = StringIO(u'{"key": "value"}')
+
+        backdrop.send(["--url", "http://backdrop-bucket-url",
+                       "--token", "bucket-auth-token"], input=data_to_send)
+
+        self.assert_data_got_sent(post)
+
+    def assert_data_got_sent(self, post):
         post.assert_called_with(
             url="http://backdrop-bucket-url",
             data=u'{"key": "value"}',
