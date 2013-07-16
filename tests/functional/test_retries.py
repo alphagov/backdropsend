654398da3dd75253de9a4c35286781b50cc8966f
Paulo Schneider <paulo.schneider@gmail.com>
2013-07-16 17:25:43 +0100

    Splitting tests into functional features.
    @alexmuller, @phss

---
+++ tests/functional/test_retries.py
@@ -0,0 +1,43 @@
+import tempfile
+import unittest
+from hamcrest import *
+from tests.functional import command
+from tests.functional.http_stub import HttpStub
+
+
+class TestRetries(unittest.TestCase):
+    def setUp(self):
+        HttpStub.start()
+
+    def tearDown(self):
+        HttpStub.stop()
+
+    def test_it_passes_after_default_number_of_retries(self):
+        HttpStub.set_response_codes(500, 500, 200)
+        cmd = command.do("./backdrop-send "
+                         "--url http://localhost:8000/bucket "
+                         "--sleep 0 "
+                         "--token bucket-auth-token", stdin='{"key": "value"}')
+
+        assert_that(cmd.exit_status, is_(0))
+
+    def test_it_passes_after_specified_number_of_retries(self):
+        HttpStub.set_response_codes(500, 500, 500, 200)
+        cmd = command.do("./backdrop-send "
+                         "--url http://localhost:8000/bucket "
+                         "--token bucket-auth-token "
+                         "--sleep 0 "
+                         "--attempts 4", stdin='{"key": "value"}')
+
+        assert_that(cmd.exit_status, is_(0))
+
+    def test_it_fails_after_specified_number_of_retries(self):
+        HttpStub.set_response_codes(500, 500, 200)
+        cmd = command.do("./backdrop-send "
+                         "--url http://localhost:8000/bucket "
+                         "--token bucket-auth-token "
+                         "--sleep 0 "
+                         "--attempts 2", stdin='{"key": "value"}')
+
+        assert_that(cmd.exit_status, is_not(0))
+
