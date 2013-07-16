cee985c1eb1456d73ab0bfdd5e0fa9f7e655ad72
Paulo Schneider <paulo.schneider@gmail.com>
2013-07-16 21:49:19 +0100

    Extracted status to a class and using a generator to simplify sending logic

--- backdrop/send.py
+++ backdrop/send.py
@@ -34,15 +34,28 @@ def parse_args(args, input):
 
     return arguments
 
-def status(message, code):
-  return { 'code': code, 'message': message, 'details': {} }
+class Status():
+    def __init__(self, message, code):
+        self.code = code
+        self.message = message
+        self.details = {}
+  
+    def description(self):
+        return self.message.format(**self.details)
+  
+    def with_details(self, details):
+        self.details = details
+        return self
+  
+    def is_ok(self):
+        return self.code == 0
 
-OK = status("", 0)
-UNAUTHORIZED = status("Unable to send to backdrop. "
+OK = Status("", 0)
+UNAUTHORIZED = Status("Unable to send to backdrop. "
                 "Unauthorised: check your access token.", 4)
-HTTP_ERROR = status("Unable to send to backdrop. Server responded with {status}. "
+HTTP_ERROR = Status("Unable to send to backdrop. Server responded with {status}. "
               "Error: {message}.", 8)
-CONNECTION_ERROR = status("Unable to send to backdrop. Connection error.", 16)
+CONNECTION_ERROR = Status("Unable to send to backdrop. Connection error.", 16)
 
 def log(message):
     print >> sys.stderr, message
@@ -52,8 +65,7 @@ def handle_response(response):
         return UNAUTHORIZED
 
     if response.status_code < 200 or response.status_code >= 300:
-        HTTP_ERROR['details'] = { 'status': response.status_code, 'message': response.text }
-        return HTTP_ERROR
+        return HTTP_ERROR.with_details({ 'status': response.status_code, 'message': response.text })
     
     return OK
 
@@ -68,9 +80,7 @@ def post(data, arguments):
     except (requests.ConnectionError, requests.exceptions.Timeout) as e:
         return CONNECTION_ERROR
 
-def send(args, input=None):
-    arguments = parse_args(args, input)
-
+def post_attempts(arguments):
     data = arguments.file.read()
     attempts = arguments.attempts
 
@@ -78,16 +88,23 @@ def send(args, input=None):
         attempts = 1
 
     for i in range(attempts):
-        last_retry = i == (attempts - 1)
+        last_attempt = i == (attempts - 1)
         
         status = post(data, arguments)
 
-        log(status['message'].format(**status['details']))
-
-        if status['code'] == 0 or last_retry:
-            break
+        yield status, last_attempt
 
         log("Retrying...")
         time.sleep(arguments.sleep)
 
-    exit(status['code'])
+
+def send(args, input=None):
+    arguments = parse_args(args, input)
+
+    for status, is_last_attempt in post_attempts(arguments):
+        log(status.description())
+
+        if status.is_ok() or is_last_attempt:
+            break
+
+    exit(status.code)
